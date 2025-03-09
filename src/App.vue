abaf6580b9aac5f88e93b3cef358adeff2398dc1
Appokaliptik <alexanderstrannik@gmail.com>
2025-03-09 15:20:35 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import VirtList from './components/virtList.vue'
+</script>
+
+<template>
+  <header>
+    <VirtList />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
